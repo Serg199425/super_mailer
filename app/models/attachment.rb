f8a8fe6f5c361614d30bfed3b4bcb2b29210ca0e
serg199425 <serg199425@tut.by>
2015-09-03 04:23:56 +0300

    added attach file to letter

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,8 @@
 class Attachment < ActiveRecord::Base
   belongs_to :letter
 
-  has_attached_file :file, :path => ":rails_root/public/attachments/user_:user_id/message_id_:message_id/:basename.:extension"
+  has_attached_file :file, :url => "/attachments/:basename.:extension",
+                    :path => ":rails_root/public/attachments/user_:user_id/message_id_:message_id/:basename.:extension"
   do_not_validate_attachment_file_type :file
 
   Paperclip.interpolates :user_id do |attachment, style|
--- app/models/letter.rb
+++ app/models/letter.rb
@@ -17,19 +17,21 @@ class Letter < ActiveRecord::Base
     return unless self.valid?
     provider_account = self.provider_account
     Mail.defaults do
-      delivery_method :smtp, address: "smtp.yandex.ru", port: 465, tls: true,
+      delivery_method :smtp, address: provider_account.address, port: 465, tls: true,
                       user_name: provider_account.login, password: provider_account.password 
     end
 
     mail = Mail.new(from: self.from, to: self.to, subject: self.subject )
 
+    self.message_id = mail.message_id
+
     mail.html_part = Mail::Part.new(content_type: 'text/html; charset=UTF-8', body: self.body)
+
+    self.attachments.each { |attachment| mail.add_file attachment.file.path }
 
     if mail.deliver!
       self.group = :outbox
-      return true
-    else
-      return false
+      self.date = mail.date
     end
   end
 
